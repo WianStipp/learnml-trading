fb57137a846e0cb3a0c2ec23be92fa58267dc1cf
WianStipp <stippwian@gmail.com>
2021-03-30 07:38:34 +0100

    added documentation setup

--- learnmltrading/broker/broker.py
+++ learnmltrading/broker/broker.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from abc import ABC
 import pandas as pd
 
 
 class Broker(ABC):
+    """
+    Abstract class for a broker.
+    """
+
     def __init__(self, host: str, port: str) -> None:
         self.host = host
         self.port = port
+
+
+class OandaBroker(Broker):
+    def __init__(
+        self, host: str, port: str, account_number: str, api_token: str
+    ) -> None:
+        super().__init__(host, port)
+        self.account_number = account_number
+        self.api_token = api_token
 
     def get_historic_prices(
         self, instrument: str, granularity: str, start: int, end: int
